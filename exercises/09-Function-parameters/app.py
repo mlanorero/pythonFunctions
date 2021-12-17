9a6e5bc798e5254135d21532888f12816b719088
mlanorero <mlanorero@gmail.com>
2021-12-17 01:48:39 +0000

    terminados

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,8 +1,6 @@
 # Your code goes here:
-
-def render_person(param):
-    return param
-
+def render_person(nombre,cumple,ojos,edad,sexo):
+    print(f"{nombre} is a {edad} years old {sexo} born in {cumple} with {ojos} eyes")
 
 # Do not edit below this line
-print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
+print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))           
--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,6 +1,5 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
-def sort_names(a):
-    return a.sort()
+names.sort()
 
-print(sort_names(names))
+print(names)
